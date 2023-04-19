d1ed849df77d63f776b3f47282ddeaea38703bc6
naahuel27 <Nahuelmansilla27.nm@gmail.com>
2023-04-19 11:50:28 -0300

    primer commit :)

---
+++ src/ar/edu/unlam/pb1/finalAgosto/Contacto.java
@@ -0,0 +1,40 @@
+package ar.edu.unlam.pb1.finalAgosto;
+
+public class Contacto {
+	
+	/*
+	 * Se deben incorporar los atributos necesarios.
+	 * 
+	 * •	Nombre y Apellido: No se aceptan números.
+	 * •	Celular: Compuesto del código de país + código de área + número de celular.
+	 * •	E-Mail: Debe contener al menos el símbolo ‘@’ y el caracter ‘.’.
+	 * •	Dirección: Valor alfanumérico.
+	 * •	Código Postal: Valor numérico.
+	 * •	Localidad: Valor alfanumérico.
+	 * •	Provincia. Enumerador que contenga las 23 provincias argentinas.
+	 * •	Es cliente (Si o No): Inicialmente se carga en “No”.
+	 * •	Desea ser llamado nuevamente (Si o No): Inicialmente se carga en “Si”.
+	 */
+	
+	public void esEmailValido(String eMail) {
+		/*
+		 * Evalúa si un String determinado puede ser almacenado como E-MAIL.
+		 */
+	}
+	
+	public boolean registrarNuevaLlamada(Llamada nueva) {
+		/*
+		 * Registra una nueva llamada asociada al contacto actual.
+		 */
+		return false;
+	}
+	
+	public String toString() {
+		/*
+		 * Muestra los datos de un contacto, entre los que se debe incluir el registro de las llamadas realizadas.
+		 */
+		
+		return "";
+	}
+	
+}
---
+++ test/ar/edu/unlam/pb1/finalAgosto/InterfazCallCenter.java
@@ -0,0 +1,48 @@
+package ar.edu.unlam.pb1.finalAgosto;
+
+public class InterfazCallCenter {
+	
+	public static void main(String args[]) {
+			
+		System.out.println("Bienvenido al callcenter");
+	
+		System.out.println("************************");
+		System.out.println("Menu de opciones\n");
+		System.out.println("1 - Incorporar zona de cobertura");
+		System.out.println("2 - Dar de alta un nuevo contacto");
+		System.out.println("3 - Realizar nueva llamada");
+		System.out.println("4 - Ver información del contacto");
+		System.out.println("9 - Salir");
+		System.out.println("************************");
+	}
+	
+	private static void incorporarZonaDeCobertura() {
+		/*
+		 * Se registra un nuevo código postal dentro de la zona de cobertura de la empresa
+		 */
+	}
+	
+	private static void darDeAltaNuevoContacto() {
+		/*
+		 * Registra un nuevo contacto en la empresa
+		 */
+	}
+	
+	private static void realizarNuevaLlamada() {
+		/*
+		 * Busca un candidato,  muestra los datos del mismo, y permite almacenar el resultado de la llamada.
+		 * 
+		 * a.	Si la llamada fue exitosa (en ese caso el contacto pasa a ser cliente, y no se lo debe volver a 
+		 * llamar).
+		 * b.	Si el contacto no desea ser llamado nuevamente (la llamada pudo no haber sido exitosa, pero se haga 
+		 * un nuevo intento en el futuro).
+		 * c.	Observaciones: Texto libre donde el operador deja registro de lo conversado.
+		 */
+	}
+	
+	private static void verInformacionDelContacto() {
+		/*
+		 * Se visualiza la información del contacto, incluso el listado de las llamadas que se le hicieron
+		 */
+	}
+}
